770fa9d9b2cbf5fcd1488520c25aaa6c4bef5a59
John Jacquay <john@bioteam.net>
2020-05-22 13:16:48 -0600

    Webpack config

--- config-overrides.js
+++ config-overrides.js
@@ -4,7 +4,10 @@ const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
 module.exports = function override(config, env) {
     config.plugins = [
         ...config.plugins,
-        new MonacoWebpackPlugin()
+        new MonacoWebpackPlugin({
+            // available options are documented at https://github.com/Microsoft/monaco-editor-webpack-plugin#options
+            languages: ['json']
+        })
     ];
 
     return config;
